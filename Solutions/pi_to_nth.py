6b2ff9a1650f7ca9e6a647e0f35a645415f443cb
Jennifer Garst <jennifer.garst@gmail.com>
2020-02-09 10:22:11 -0600

    Modifying gauss_legendre function

--- Solutions/pi_to_nth.py
+++ Solutions/pi_to_nth.py
@@ -1,22 +1,39 @@
+
+"""Print value of Pi to user-supplied precision.
+
+    Usage:
+        Runs from command line and prompts user for integer between 0 and 1000.
+        Prints Pi to specified precision with no rounding.
+"""
+
 from decimal import Decimal as D, getcontext
 
+
 def gauss_legendre(precision):
+    """Returns a Decimal value for pi to specified number of digits, without rounding
+
+            Args:
+                precision: number of digits to return after the decimal point
+    """
     getcontext().prec = precision + 2
-    a = [1]
-    b = [1 / D(2).sqrt()]
-    t = [D(.25)]
-    p = [1]
+    a0 = 1
+    b0 = 1 / D(2).sqrt()
+    t0 = D(.25)
+    p0 = 1
     c = 1
     while c <= (precision // 2) + 1:
-        a.append((a[c - 1] + b[c - 1]) / D(2))
-        b.append(D(a[c - 1] * b[c - 1]).sqrt())
-        t.append(t[c - 1] - p[c - 1] * ((a[c - 1] - a[c]) ** 2))
-        p.append(2 * p[c - 1])
+        an = (a0 + b0) / D(2)
+        bn = D(a0 * b0).sqrt()
+        tn = t0 - p0 * ((a0 - an) ** 2)
+        pn = 2 * p0
+        a0, b0, t0, p0 = an, bn, tn, pn
         c += 1
-    pi_calc = ((a[-1] + b[-1]) ** D(2)) / (4 * t[-1])
+    pi_calc = ((an + bn) ** D(2)) / (4 * tn)
     return D(str(pi_calc)[0:-1])
 
+
 if __name__ == "__main__":
+    """Prompts the user for an integer between 0 and 1000 and prints pi to the specified number of digits after the decimal point"""
     while True:
         num = input("Please enter the precision you want:")
         if num.isdigit() and int(num) <= 1000:
